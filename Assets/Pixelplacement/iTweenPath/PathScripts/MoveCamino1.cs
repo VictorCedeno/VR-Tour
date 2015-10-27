24c0f8d5d9b2b1f1a77c4a6ed5048f8f4d663936
VictorCedeno <victor1006@hotmail.com>
2015-10-26 20:59:24 -0500

    CommiTest

---
+++ Assets/Pixelplacement/iTweenPath/PathScripts/MoveCamino1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class MoveCamino1 : MonoBehaviour
+{	
+	public bool camino1Flag = true;
+	void Update(){
+		if (camino1Flag){
+			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Camino1"), "easetype", iTween.EaseType.easeInOutSine, "time", 5));
+		}
+
+	}
+}
---
+++ Assets/Scripts/Autowalk.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Autowalk : MonoBehaviour {
+	
+	private CardboardHead head;
+	private Vector3 startingPosition;
+	private float delay = 0.0f; 
+
+	public MoveCamino1 boolBoy;
+	
+	void Start() {
+		head = Camera.main.GetComponent<StereoController>().Head;
+		startingPosition = transform.localPosition;
+	}
+	
+	void Update() {
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		// if looking at object for 2 seconds, enable/disable autowalk
+		if (isLookedAt && Time.time>delay) { 
+			GameObject g = GameObject.FindGameObjectWithTag ("Player");
+			boolBoy = g.GetComponent<MoveCamino1> ();
+			// accesses the bool named "isOnFire" and changed it's value.
+			boolBoy.camino1Flag= !boolBoy.camino1Flag;
+			delay = Time.time + 1.0f;
+		}
+		// currently looking at object
+		else if (isLookedAt) { 
+			GetComponent<Renderer>().material.color = Color.yellow; 
+		} 
+		// not looking at object
+		else if (!isLookedAt) { 
+			GetComponent<Renderer>().material.color = Color.red; 
+			delay = Time.time + 1.0f; 
+		}
+	}
+	
+}
