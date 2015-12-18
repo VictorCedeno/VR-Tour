1b673ac01f327d2338557fe6412ed090f013f46a
Denny Schuldt <dschuldt@outlook.com>
2015-12-18 00:03:56 -0500

    Photosphere viewer :bulb:

---
+++ Assets/Cardboard/PhotosphereScenes/Course/OnBackPressed.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnBackPressed : MonoBehaviour {
+
+	private CardboardHead head;
+	private float delay = 0.0f;
+	
+	void LoadScene() {
+		Application.LoadLevel(0);
+	}
+	
+	void Start() {
+		head = Camera.main.GetComponent<StereoController>().Head;
+	}
+	
+	void Update() {
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		// if looking at object for 2 seconds, enable/disable autowalk
+		if (isLookedAt && Time.time>delay) { 
+			LoadScene();
+			delay = Time.time + 2.0f;
+		}
+		// currently looking at object
+		else if (isLookedAt) { 
+			head.BroadcastMessage("looking");
+		} 
+		// not looking at object
+		else if (!isLookedAt) { 
+			delay = Time.time + 2.0f; 
+		}
+	}
+}
